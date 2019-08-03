beeb5b139c5c3d2ea48a1bf67fa38fd124e8c0e8
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-08-03 20:36:30 -0300

    Implementação de teste no componente comment

--- src/Comment.js
+++ src/Comment.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Comment = ({c}) => {
+    let comment = 'vazio'
+
+    if (c && c.comment){
+        comment = c.comment
+    }
+
     return (
-        <div>{c.comment}</div>
+        <div>{`Comentário: ${comment}`}</div>
     )
 }
 
