78c02f334b985e2353834b964418d6f218032123
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2019-08-05 00:07:34 -0300

    Correção de bug no envio da mensagem

--- src/NewComment.js
+++ src/NewComment.js
@@ -13,21 +13,24 @@ class NewComment extends Component {
     }
 
     sendComment = () => {
-        this.props.sendComment(this.state.newComment);
-        this.setState({
-            newComment: ''
-        })
+        if (this.state.newComment !== '') {
+            this.props.sendComment(this.state.newComment);
+            this.setState({
+                newComment: ''
+            })
+        }
     }
 
     render() {
         return (
             <div>
-                <InputGroup value={this.state.newComment} onChange={this.handleChange}>
-                    <FormControl placeholder="Digite seu comentário..." as="textarea" aria-label="With textarea" />
+                <InputGroup onChange={this.handleChange}>
+                    <FormControl placeholder="Digite seu comentário..." as="textarea" value={this.state.newComment}/>
                     <Button onClick={this.sendComment} className='ml-1'>
                         <InputGroup.Prepend>Enviar</InputGroup.Prepend>
                     </Button>
                 </InputGroup>
+                {JSON.stringify(this.state.newComment)}
             </div>
         );
     }
